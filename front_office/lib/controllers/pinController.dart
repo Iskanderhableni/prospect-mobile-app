8505194170bc0da84d6bf2d44c69ab0e0cf6f8c9
Iskanderhableni <89342940+Iskanderhableni@users.noreply.github.com>
2023-05-18 01:48:16 +0100

    pinController.dart

--- front_office/lib/controllers/pinController.dart
+++ front_office/lib/controllers/pinController.dart
@@ -1,48 +1,4 @@
 // ignore_for_file: file_names, unrelated_type_equality_checks
-
-/*import 'dart:convert';
-
-import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:http/http.dart' as http;
-import '../models/pin_model.dart';
-
-class PinController extends GetxController {
-  Rx<List<PinModel>> pins = Rx<List<PinModel>>([]);
-  final box = GetStorage();
-  final isLoading = false.obs;
-
-  String url = 'http://192.168.219.167:80/api/pins';
-  @override
-  void onInit() {
-    getAllPins();
-    super.onInit();
-  }
-
-  Future getAllPins() async {
-    try {
-      isLoading.value = true;
-      var response = await http.get(
-        Uri.parse('http://192.168.219.167:80/api/pins'),
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Bearer ${box.read('token')}',
-        },
-      );
-      if (response.statusCode == 200) {
-        for (var item in json.decode(response.body)['pins']) {
-          pins.value.add(PinModel.fromJson(item));
-        }
-      } else {
-        isLoading.value = false;
-        print(json.decode(response.body));
-      }
-    } catch (e) {
-      isLoading.value = false;
-      print(e.toString());
-    }
-  }
-}*/
 import 'dart:convert';
 import '../models/pin_model.dart';
 import 'package:get/get.dart';
@@ -52,7 +8,7 @@ import 'package:http/http.dart' as http;
 class PinController extends GetxController {
   Rx<List<PinModel>> pins = Rx<List<PinModel>>([]);
   final isLoading = false.obs;
-  String url = '192.168.156.167:80/api/';
+  String url = 'URL/api/';
 
   final box = GetStorage();
 
